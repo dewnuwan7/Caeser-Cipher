fffe863e72ab5e584cac602088c753f2b9c59adb
Dewnuwan Jayaweera <72617382+dewnuwan7@users.noreply.github.com>
2021-01-10 03:39:47 -0500

    Add files via upload

---
+++ caesar cipher.py
@@ -0,0 +1,51 @@
+
+def caesar(userText,shiftAmount,cipherDirection):
+    endText = ""
+    for char in userText:
+        if char in alphabet:
+            position = alphabet.index(char)
+            if cipherDirection == 'decode':
+               new_position = position - shiftAmount
+            else:
+                new_position = position + shiftAmount
+            endText += alphabet[new_position]
+        else:
+            endText += char
+
+    print(f"'{endText}' is the {cipherDirection}d text.")     
+    restart = input("Type 'yes' to rerun. Otherwise type 'no'\n")
+    if restart =='yes':
+        start = True
+    else:
+        print("Good Bye!")
+        exit()
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print("""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,s88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+)
+
+start = True
+while start == True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt.\n")
+    text = input("Type your message:").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 25
+    caesar(userText = text,shiftAmount = shift,cipherDirection = direction)
